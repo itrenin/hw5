3599be1d1b33bb614e97aabb9e110ad4a2dc5238
Ilya Trenin <itrenin@gmail.com>
2020-04-18 19:54:54 +0300

    закончен чат

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 require('./models/connection')
+const Chat = require('./models/schemas/chat')
 
 // parse application/json
 app.use(express.json())
@@ -34,27 +35,72 @@ app.use((err, _, res, __) => {
     data: { status: 500, message: err.message },
   })
 })
+// реализация чата
+let users = []
+io.sockets.on('connection', (socket) => {
+  //console.log(socket.handshake)
+  socket.on('users:connect', ({ userId, username }) => {
+    // const user = { [socket.id]: {
+    //   username: username,
+    //   socketId: socket.id,
+    //   userId: userId,
+    //   activeRoom: null // По умолчанию
+    //   }
+    // }
+    const user = {
+      username: username,
+      socketId: socket.id,
+      userId: userId,
+      activeRoom: null, // По умолчанию
+    }
 
-io.on('users:connect', (socket) => {
-  const user = {
-    #socketId: {
-      username: '',
-      socketId: '',
-      userId: '',
-      activeRoom: null,
-    },
-  }
-  socket.emit('users:list)', user)
-  socket.broadcast.emit('users:add', user)
-})
+    users.push(user)
 
-io.on('message:add', (socket) => {})
-io.on('message:history', () => {
-  console.log('hustory!')
+    socket.emit('users:list', users)
+    socket.broadcast.emit('users:add', user)
+  })
+  socket.on('message:add', async (message) => {
+    let dialog = new Object()
+    const newMessage = new Chat(message)
+    await newMessage.save()
+
+    users.forEach((user) => {
+      if (user.userId === message.recipientId) dialog.to = user.socketId
+      if (user.userId === message.senderId) dialog.from = user.socketId
+    })
+
+    io.to(dialog.from).to(dialog.to).emit('message:add', message)
+  })
+
+  socket.on('message:history', async (dialog) => {
+    const findHistory = (dialog, msg) => {
+      if (
+        (dialog.recipientId === msg.recipientId &&
+          dialog.userId === msg.senderId) ||
+        (dialog.recipientId === msg.senderId &&
+          dialog.userId === msg.recipientId)
+      ) {
+        return true
+      }
+      return false
+    }
+
+    const history = (await Chat.find()).filter((msg) =>
+      findHistory(dialog, msg) ? true : false
+    )
+
+    socket.emit('message:history', history)
+  })
+  socket.on('disconnect', (data) => {
+    socket.broadcast.emit('users:leave', users[socket.id])
+
+    users.forEach((user, index) => {
+      if (user.socketId === socket.id) users.splice(index, 1)
+    })
+  })
 })
-io.on('disconnect', () => {
-  console.log('disconnected!')
-})
+// конец чата. Надо вынети в отдельный модуль
+
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
